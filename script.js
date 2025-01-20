46c240bb55b5557ab6dddfc05f9383d9fa226696
Hyebin Seo <teorgeos@gmail.com>
2025-01-20 22:41:16 +0900

    add dividing in javascript and html

--- script.js
+++ script.js
@@ -11,6 +11,27 @@ function plusHandler() {
     $("#result").text(result);
 }
 
+function divideHandler() {
+
+    var firstNumber = $("#first").val();
+    var secondNumber = $("#second").val();
+    var thirdNumber = $("#third").val();
+
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+    thirdNumber = parseFloat(thirdNumber);
+    var result = firstNumber * 0.42 + secondNumber * 0.42 + thirdNumber * 0.42;
+    $("#result").text(result);
+    
+    var peopleNumber = $("#people").val();
+    
+    //peopleNumber = parseFloat(peopleNumber);
+
+    var result2 = result / peopleNumber;
+    $("#result2").text(result2);
+}
+
 $(document).ready(function() {
     $("#plus").click(plusHandler);
+    $("#divide").click(divideHandler);
 });
