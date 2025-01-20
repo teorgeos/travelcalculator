77096ebb00fa38d6092bfc37b28d94b2396b5c3a
Hyebin Seo <teorgeos@gmail.com>
2025-01-20 22:27:36 +0900

    add javascript for calculating mongolian currency unit into korean currency unit

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
+function plusHandler() {
+    /* if you click the button, */
+    var firstNumber = $("#first").val();
+    var secondNumber = $("#second").val();
+    var thirdNumber = $("#third").val();
 
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
+    thirdNumber = parseFloat(thirdNumber);
+    var result = firstNumber * 0.42 + secondNumber * 0.42 + thirdNumber * 0.42;
+    $("#result").text(result);
+}
+
+$(document).ready(function() {
+    $("#plus").click(plusHandler);
 });
